07805b59f8bae330428b539c8b453e441c749e4d
Aarin Deb <aarindeb2003@gmail.com>
2026-07-18 04:32:17 +0200

    Config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Plateforme Artisans Auvergne-Rhône-Alpes</h1>
+      <p>L'environnement React, TypeScript et Sass est prêt !</p>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/main.scss';
+
+// Le point d'exclamation après getElementById dit à TS que l'élément existe bien
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
