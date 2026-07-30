cdb6f40bc0b3e24aa9e50e59eaad7d1ff24db433
Aarin Deb <aarindeb2003@gmail.com>
2026-07-30 13:54:09 +0200

    Correction sémantique des titres Cartes Artisan

--- src/components/ArtisanCard.tsx
+++ src/components/ArtisanCard.tsx
@@ -25,7 +25,7 @@ export default function ArtisanCard({
     // 1. On stocke le visuel de la carte dans une constante
     const cardContent = (
         <article className="artisan-card">
-        <h3 className="artisan-name">{name}</h3>
+        <h2 className="artisan-name">{name}</h2>
         
         <div className="artisan-rating" role="img" aria-label={`Note de ${ratingValue} sur 5`}>
             {[...Array(5)].map((_, index) => (
@@ -34,7 +34,7 @@ export default function ArtisanCard({
                 className={`star ${index < ratingValue ? 'active' : ''}`} 
                 size={20} 
             />
-            ))}
+            ))}     
         </div>
         
         <p className="artisan-category"><strong>{specialty}</strong></p>
